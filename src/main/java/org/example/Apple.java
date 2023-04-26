b0125c97e04e7588ed8e559c99e42187a0fd58c8
kaifler <kaifler@mail.ru>
2023-04-26 19:44:09 +0500

    hw3

---
+++ src/main/java/org/example/Apple.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Apple extends Fruit {
+    public Apple(float weight) {
+        super(1);
+    }
+}
---
+++ src/main/java/org/example/Fruit.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public abstract class Fruit {
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
